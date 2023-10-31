1a6f9c59625f2800538d0fa0e4e84115af3bdcad
FouziD <144001834+FouziD@users.noreply.github.com>
2023-10-31 14:19:27 +0100

    me

---
+++ Assets/scripts/ball2.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball2 : MonoBehaviour
+{
+
+    public float Xposition = 0f;
+    public float Yposition = 0f;
+    public float xSpeed;
+    public float ySpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(Xposition, Yposition, 0);
+        xSpeed = 20f;
+        ySpeed = 20f;
+    }
+    private Rigidbody2D _rigidbody;
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        Xposition = Xposition + xSpeed * Time.deltaTime;
+        Yposition = Yposition + ySpeed * Time.deltaTime;
+        transform.position = new Vector3(Xposition, Yposition, 0);
+    }
+
+
+
+    // Update is called once per frame
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("horizontalWall") == true)
+        {
+            ySpeed = ySpeed * -1f;
+        }
+
+        else if (collision.gameObject.CompareTag("verticaltalWall"))
+        {
+            xSpeed = xSpeed * -1f;
+        }
+
+        else if (collision.gameObject.CompareTag("Player"))
+        {
+            xSpeed = xSpeed * -1.1f;
+        }
+    }
+}
--- Assets/scripts/collision.cs
+++ Assets/scripts/collision.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 using TMPro;
 public class collision : MonoBehaviour
 {
-    public float Xposition = 0f;
-    public float Yposition = 0f;
+
     public float xSpeed = 1f;
     public float ySpeed = 1f;
     public TMP_Text scoreField;
     private int leftScore = 0;
     private int rightScore = 0;
     public int topScore = 1;
+    
 
     private void resetBall(string leftOrRight)
     {
-        Xposition = 0f;
-        Yposition = 0f;
+        GetComponent<ball>().Xposition = 0f;
+        GetComponent<ball>().Yposition = 0f;
         scoreField.text = leftScore + " - " + rightScore;
         if (leftOrRight == "left")
         {
@@ -30,33 +30,22 @@ public class collision : MonoBehaviour
         }
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        //Xposition = Random.Range(-10.0f, 10.0f);
-        transform.position = new Vector3(Xposition, Yposition, 0);
-
-    }
-
     // Update is called once per frame
     void Update()
     {
 
+        
         if (leftScore >= topScore)
         {
             scoreField.text = "Left Player has won!";
             xSpeed = 0;
             ySpeed = 0;
-            Xposition = 0f;
-            Yposition = 0f;
         }
         else if (rightScore >= topScore)
         {
             scoreField.text = "Right Player has won!";
             xSpeed = 0;
             ySpeed = 0;
-            Xposition = 0f;
-            Yposition = 0f;
         }
     }
 
